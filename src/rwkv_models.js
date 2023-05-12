3e564479086a379332726f63254b975fe66312ac
Eugene Cheah (@picocreator) <eugeneqin@gmail.com>
2023-05-12 18:30:59 +0800

    more RWKV models

--- src/rwkv_models.js
+++ src/rwkv_models.js
@@ -1,34 +1,48 @@
 module.exports = [
 	{
-		label: 'raven 1B5 v11 (Small, Fast, Dumb)',
+		label: 'raven 1B5 v11 (Smaller, Faster, Dumber)',
 		name: "raven_1b5_v11.bin",
 		url: "https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-1B5-v11.bin",
 		sha256: "098c6ea8368f68317c99283195651685bdaac417857a21e447eadced2e62f8eb",
 		size: 3031328341
 	},
 	{
-		label: 'raven 3B v11 (Small, Fast)',
+		label: 'raven 3B v11 (Smaller, Faster)',
 		name: "raven_3b_v11.bin",
 		url: "https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-3B-v11.bin",
 		sha256: "20c1cbb2fce41a0e045d866ebb6766153c4f62cc8fee89091b61eae6603b6969",
 		size: 5971101117
 	},
 	{ 
-		label: 'raven 7B v11 (Q8_0)', 
-		name: "raven_7b_v11_Q8_0.bin",
-		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-7B-v11x-Eng99%25-Other1%25-20230429-ctx8192.bin',
-		sha256: '75d252da63405e9897bff2957f9b6b1c94d496a50e4772d5fc1ec22fb048f9b5',
-		size: 8681332157
+		label: 'raven 7B v11 (Q4_2, Small, Fast)', 
+		name: "raven_7b_v11_Q4_2.bin",
+		url: 'https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-7B-v11x-Q4_2.bin',
+		sha256: '0ed3e59bc123ec84a561948e0643809ccc73d7900ee0078fe48b13bfbcb7711f',
+		size: 5191671229
 	},
 	{ 
-		label: 'raven 7B v11x', 
+		label: 'raven 7B v11 (Q5_1, Small, Fast)', 
+		name: "raven_7b_v11_Q5_1.bin",
+		url: 'https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-7B-v11x-Q5_1.bin',
+		sha256: '24179ff3b59635a8b90cca90533ca3163392c28376368fdd19b0b8929bb6abdf',
+		size: 6064086461
+	},
+	{ 
+		label: 'raven 7B v11x (Small, Fast)', 
 		name: "raven_7b_v11x.bin",
 		url: 'https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-7B-v11x.bin',
 		sha256: '9e6886514a5c1162b760ae6ce873e71b8934d7e457b680b8cd7dcbfc4e118c8f',
 		size: 14788238781
 	},
 	{ 
-		label: 'raven 14B v11 (Q8_0, Bigger, better)',
+		label: 'raven 14B v11x (Q5_1, Bigger, better)',
+		name: "raven_14b_v11_Q5_1.bin",
+		url: 'https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-14B-v11x-Q5_1.bin',
+		sha256: '54e8a15af5ea44fabd553720570c84d573267b045bcd2f9c7c53a58afca7c2a0',
+		size: 11262412069
+	},
+	{ 
+		label: 'raven 14B v11x (Q8_0, Bigger, better)',
 		name: "raven_14b_v11_Q8_0.bin",
 		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-14B-v11x-Eng99%25-Other1%25-20230501-ctx8192.bin',
 		sha256: '1cb56cd7784264a8f0ed2efd17a97f4866075915f2b1a8497c1abb826edade8b',
@@ -42,14 +56,21 @@ module.exports = [
 		size: 28301772069
 	},
 	{
-		label: 'raven 3B v11 (Small, Fast, multilingual)',
+		label: 'raven 3B v11 (Smaller, Faster, multilingual)',
 		name: "raven_3b_v11_multilingual.bin",
 		url: "https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-3B-v11-multilingual.bin",
 		sha256: "df91cd6962340f9d5610b2fb51cc134de43a356bb9830c41a2bc6cd81cd2703d",
 		size: 5971101117
 	},
 	{ 
-		label: 'raven 7B v11 (Q8_0, multilingual)', 
+		label: 'raven 7B v11 (Q8_0, Small, Fast)', 
+		name: "raven_7b_v11_Q8_0.bin",
+		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-7B-v11x-Eng99%25-Other1%25-20230429-ctx8192.bin',
+		sha256: '75d252da63405e9897bff2957f9b6b1c94d496a50e4772d5fc1ec22fb048f9b5',
+		size: 8681332157
+	},
+	{ 
+		label: 'raven 7B v11 (Q8_0, Small, Fast, multilingual)', 
 		name: "raven_7b_v11_Q8_0_multilingual.bin",
 		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-7B-v11-Eng49%25-Chn49%25-Jpn1%25-Other1%25-20230430-ctx8192.bin',
 		sha256: 'ee4a6e7fbf9c2bd3558e4a92dbf16fd25d8599c7bef379ba91054981a8f665e0',
