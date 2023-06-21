56e6bd32278dfc2c253e042805bad5e697e3b240
picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-06-21 23:45:11 +0800

    model list tweak

--- src/rwkv_models.js
+++ src/rwkv_models.js
@@ -1,14 +1,14 @@
 module.exports = [
 	{
 		label: 'raven 1B5 v12 (Smaller, Faster, Dumber)',
-		name: "raven_1b5_v11.bin",
+		name: "raven_1b5_v12.bin",
 		url: "https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q4_0-RWKV-4-Raven-1B5-v12-Eng98%25-Other2%25-20230520-ctx4096.bin",
 		sha256: "46a66fc90b072536d835922d7565706671a2c8a7d2cdfb2b4a33814e45612fad",
 		size: 1562052181
 	},
 	{
 		label: 'raven 3B v12 (Smaller, Faster)',
-		name: "raven_3b_v11.bin",
+		name: "raven_3b_v12.bin",
 		url: "https://huggingface.co/latestissue/rwkv-4-raven-ggml-quantized/resolve/main/q5_1-RWKV-4-Raven-3B-v12-Eng98%25-Other2%25-20230520-ctx4096.bin",
 		sha256: "20c1cbb2fce41a0e045d866ebb6766153c4f62cc8fee89091b61eae6603b6969",
 		size: 5971101117
@@ -36,16 +36,16 @@ module.exports = [
 	},
 /*	
 	{
-		label: 'raven 3B v11 (Smaller, Faster, multilingual)',
-		name: "raven_3b_v11_multilingual.bin",
-		url: "https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-3B-v11-multilingual.bin",
+		label: 'raven 3B v12 (Smaller, Faster, multilingual)',
+		name: "raven_3b_v12_multilingual.bin",
+		url: "https://huggingface.co/datasets/picocreator/rwkv-4-cpp-quantize-bin/resolve/main/RWKV-4-Raven-3B-v12-multilingual.bin",
 		sha256: "df91cd6962340f9d5610b2fb51cc134de43a356bb9830c41a2bc6cd81cd2703d",
 		size: 5971101117
 	},
 	{ 
-		label: 'raven 7B v11 (Q8_0, Small, Fast, multilingual)', 
-		name: "raven_7b_v11_Q8_0_multilingual.bin",
-		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-7B-v11-Eng49%25-Chn49%25-Jpn1%25-Other1%25-20230430-ctx8192.bin',
+		label: 'raven 7B v12 (Q8_0, Small, Fast, multilingual)', 
+		name: "raven_7b_v12_Q8_0_multilingual.bin",
+		url: 'https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/Q8_0-RWKV-4-Raven-7B-v12-Eng49%25-Chn49%25-Jpn1%25-Other1%25-20230430-ctx8192.bin',
 		sha256: 'ee4a6e7fbf9c2bd3558e4a92dbf16fd25d8599c7bef379ba91054981a8f665e0',
 		size: 8681332157
 	},
